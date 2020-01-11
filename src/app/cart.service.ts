1ed271f2c4a5ef2ac54d1ee393458873d11dd0eb
knjkmd <knjkmd@gmail.com>
2020-01-11 20:46:02 +0100

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CartService {
@@ -18,6 +19,8 @@ export class CartService {
   }
 
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
 }
